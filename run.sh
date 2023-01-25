c9d50eca759de7bdcd83d37f100625332ecf0ce7
vishvajit <vishvajitjambuti003@gmail.com>
2023-01-25 16:39:32 +0000

    run file modified

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "Ros2 workspace : $BASEDIR"
 
 echo "running colcon build"
 
-#colcon build --symlink-install
+colcon build --symlink-install
 
 echo "sourceing the local setup file for workspace"
 
